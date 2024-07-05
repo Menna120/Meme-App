30a1d63f17db3f7b937d3abd126ca1afc3133ba8
menna <94606987+Menna120@users.noreply.github.com>
2024-07-05 13:03:54 +0300

    convert to constrain layout and add motion to the name text

--- app/src/main/java/com/example/meme_app/MemeItem.kt
+++ app/src/main/java/com/example/meme_app/MemeItem.kt
@@ -3,3 +3,25 @@ package com.example.meme_app
 import androidx.annotation.DrawableRes
 
 data class MemeItem(@DrawableRes val imageResId: Int)
+
+val MEME_ITEMS = listOf(
+    MemeItem(R.drawable.image_1),
+    MemeItem(R.drawable.image_2),
+    MemeItem(R.drawable.image_3),
+    MemeItem(R.drawable.image_4),
+    MemeItem(R.drawable.image_5),
+    MemeItem(R.drawable.image_6),
+    MemeItem(R.drawable.image_7),
+    MemeItem(R.drawable.image_8),
+    MemeItem(R.drawable.image_9),
+    MemeItem(R.drawable.image_10),
+    MemeItem(R.drawable.image_11),
+    MemeItem(R.drawable.image_12),
+    MemeItem(R.drawable.image_13),
+    MemeItem(R.drawable.image_14),
+    MemeItem(R.drawable.image_15),
+    MemeItem(R.drawable.image_16),
+    MemeItem(R.drawable.image_17),
+    MemeItem(R.drawable.image_18),
+    MemeItem(R.drawable.image_19)
+)
