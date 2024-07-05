df975ba23ccaecc611d723cad458e05f7e9b5c1c
menna <94606987+Menna120@users.noreply.github.com>
2024-07-05 13:20:05 +0300

    back arrow navigation in the image full fragment

--- app/src/main/java/com/example/meme_app/MemeRecyclerFragment.kt
+++ app/src/main/java/com/example/meme_app/MemeRecyclerFragment.kt
@@ -13,27 +13,7 @@ class MemeRecyclerFragment : Fragment() {
     private var _binding: FragmentMemeRecyclerBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
-    private val memeItems = listOf(
-        MemeItem(R.drawable.image_1),
-        MemeItem(R.drawable.image_2),
-        MemeItem(R.drawable.image_3),
-        MemeItem(R.drawable.image_4),
-        MemeItem(R.drawable.image_5),
-        MemeItem(R.drawable.image_6),
-        MemeItem(R.drawable.image_7),
-        MemeItem(R.drawable.image_8),
-        MemeItem(R.drawable.image_9),
-        MemeItem(R.drawable.image_10),
-        MemeItem(R.drawable.image_11),
-        MemeItem(R.drawable.image_12),
-        MemeItem(R.drawable.image_13),
-        MemeItem(R.drawable.image_14),
-        MemeItem(R.drawable.image_15),
-        MemeItem(R.drawable.image_16),
-        MemeItem(R.drawable.image_17),
-        MemeItem(R.drawable.image_18),
-        MemeItem(R.drawable.image_19),
-    )
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,7 +26,7 @@ class MemeRecyclerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = binding.recyclerView
-        recyclerView.adapter = MemeAdapter(memeItems, parentFragmentManager)
+        recyclerView.adapter = MemeAdapter(MEME_ITEMS, parentFragmentManager)
     }
 
 }
